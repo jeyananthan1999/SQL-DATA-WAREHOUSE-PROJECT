088def25e762b558defbe8d94eed7f766215948b
Jeyananthan S <jeyananthan1999@gmail.com>
2026-04-16 15:43:17 +0530

    Create schemas for bronze, silver, and gold

--- scripts/init_database_schemas.sql
+++ scripts/init_database_schemas.sql
@@ -1,3 +1,17 @@
+/*
+=============================================================
+Create Schemas
+=============================================================
+Script Purpose:
+    -> This script sets up or creates three schemas, 'bronze', 'silver', and 'gold'. 
+    -> If those three schemas exists already, it drops and creates new one.
+	
+WARNING:
+    -> Running this script will drop the schemas if it exists already. 
+    -> All data in the databases will be permanently deleted. Proceed with caution 
+    -> Ensure you have proper backups before running this script.
+*/
+
 -- Drop databases if they already exist
 DROP DATABASE IF EXISTS bronze;
 DROP DATABASE IF EXISTS silver;
